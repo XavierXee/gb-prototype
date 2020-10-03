15fa25b9d49d948da400ed8b01146344b97cbae7
Xavier de Frutos <xavier.defrutos@gmail.com>
2020-10-04 01:32:42 +0200

    base game loop script + bg assets

---
+++ assets/backgrounds/tile-good.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="tile-good" tilewidth="8" tileheight="8" tilecount="360" columns="20">
+ <image source="tile-good.png" width="160" height="144"/>
+</tileset>
